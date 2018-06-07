bd9fb2a83c40352fdcba1963710fc40d35d57cb6
Hugo Sanchez <hugosanchez251@gmail.com>
2018-06-07 08:58:00 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,20 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = {}
+ 
   def initialize(brand)
     @brand = brand
+    #if !BRANDS[brand]
+    #  BRANDS[brand] = true
+    #end
+    BRANDS[brand] ||= true
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
+  
 
 end
