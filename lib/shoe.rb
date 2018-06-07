d6c6f30ca55272686d677b87330a2085b3af747b
Hugo Sanchez <hugosanchez251@gmail.com>
2018-06-07 08:55:58 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
+  BRANDS = {}
  
   def initialize(brand)
     @brand = brand
     if !BRANDS[brand]
-      BRANDS << brand
+      BRANDS[brand] = true
     end
   end
 
